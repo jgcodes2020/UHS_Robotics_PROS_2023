d68bbf46d957aa16f25bb56755a130c15bed44da
jgcodes2020 <jackyguo18@hotmail.com>
2023-02-20 09:31:03 -0500

    add time-based auton

--- src/lib/auton.hpp
+++ src/lib/auton.hpp
@@ -8,7 +8,7 @@
 
 namespace auton {
   // Tolerance when waiting for a motor to stop.
-  constexpr double stop_tol = 5;
+  constexpr double stop_tol = 10;
   // wheel circumference * gear ratio.
   constexpr double slide_mult = (5.0 * M_PI) * (1);
   // number of turns of leniency in position.
@@ -26,13 +26,13 @@ namespace auton {
   // distance to slide the indexer.
   constexpr double index_dist = 1.0 / index_mult;
 
-  static inline bool within_pos_tol(pros::Motor& mt, double tol) {
+  inline bool within_pos_tol(pros::Motor& mt, double tol) {
     return fabs(mt.get_position() - mt.get_target_position()) <= tol;
   }
-  static inline bool is_stopped(pros::Motor& mt) {
+  inline bool is_stopped(pros::Motor& mt) {
     return fabs(mt.get_actual_velocity()) > stop_tol;
   }
-
+  
   inline void stop() {
     motors_l.move_velocity(0);
     motors_r.move_velocity(0);
@@ -40,11 +40,31 @@ namespace auton {
       pros::delay(10);
     }
   }
+  
+  // Moves for a specific amount of time.
+  // time: time to move for
+  // vel: velocity to move at.
+  inline void slide_time(int32_t time, double vel = 170) {
+    motors_l.move_velocity(vel);
+    motors_r.move_velocity(vel);
+    pros::delay(time);
+    stop();
+  }
+  
+  // Rotates for a specific amount of time.
+  // time: time to move for
+  // vel: velocity to move at. + -> CW, - -> CCW
+  inline void rotate_time(int32_t time, double vel) {
+    motors_l.move_velocity(vel);
+    motors_r.move_velocity(-vel);
+    pros::delay(time);
+    stop();
+  }
 
   // Moves a given distance, using the motor encoders.
   // dist: distance to move, in inches
   // vel: maximum velocity to move at.
-  inline void slide(double dist, double vel = 170) {
+  inline void slide_dist(double dist, double vel = 170) {
     motors_l.move_relative(dist / slide_mult, vel);
     motors_r.move_relative(dist / slide_mult, vel);
 
@@ -53,7 +73,6 @@ namespace auton {
     )) {
       pros::delay(10);
     }
-    stop();
   }
 
   // Rotates to a given heading, based on IMU.
@@ -110,10 +129,10 @@ namespace auton {
     }
   }
 
-  // Starts or stops the flywheel.
-  // state: if true, starts the flywheel, otherwise stops it.
+  // Starts or stops the intake.
+  // state: 1, 0, or -1 (forward, stopped, backwards)
   inline void set_intake(int32_t state) {
-    motor_itk.move_velocity(-((int32_t) state) & GREEN_RPM);
+    motor_itk.move_velocity(state * GREEN_RPM);
   }
 }  // namespace auton
 
